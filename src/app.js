7ecad6f0590ff921f3ed46b5edf02aa27f230ba0
William Vargas <danloveper@gmail.com>
2025-07-05 03:57:15 +0000

    I do excuse generator whit js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function select_random(len) {
+  return Math.floor(Math.random() * len);
+}
+
+window.onload = function () {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  document.getElementById("excuse").innerHTML = `${who[select_random(who.length)]} ${action[select_random(action.length)]} ${what[select_random(what.length)]} ${when[select_random(when.length)]}`;
   console.log("Hello Rigo from the console!");
 };
